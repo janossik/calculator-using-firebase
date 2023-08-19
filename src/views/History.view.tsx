ec5796a6f4a4c30f503c0c12734472b3243ae284
janossik <marcinczaniecki97@gmail.com>
2023-08-19 02:12:30 +0200

    Added useAlert and used handleAlert in whole project for a error handle.

--- src/views/History.view.tsx
+++ src/views/History.view.tsx
@@ -4,13 +4,14 @@ import { CardCalculationMesh } from '@/components/CardCalculationMesh/CardCalcul
 import { startAfter } from 'firebase/firestore';
 
 function HistoryView() {
-  const { calculations, isAllLoaded, loadMore, last } = useCalculationData('communications');
+  const { calculations, isAllLoaded, loadMore, last, isLoading } = useCalculationData('communications');
   return (
     <Stack spacing={2} p={1}>
       <Typography variant="h1" fontSize="xx-large">
         History all users
       </Typography>
       <CardCalculationMesh
+        isLoading={isLoading}
         calculations={calculations}
         isAllLoaded={isAllLoaded}
         clickLoadMore={async () => await loadMore(startAfter(last))}
